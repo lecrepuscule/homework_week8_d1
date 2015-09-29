e4ffdfa2c87f9bab997ed7bd7398089c684ccab8
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-29 08:44:28 +0100

    users index action is working

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,12 +1,16 @@
 var express = require("express");
-var User = require("../models/user")
+var User = require("../models/user");
+var Order = require("../models/order");
 
 var usersRouter = express.Router();
 
 
 usersRouter.get("/", function(req, res){
-  User.find({}, function(err, users){
+  Order.distinct("users", function(err, users){
     if (err) console.log(err);
+    console.log("users index has " + users);
+    Array.isArray(users);
+    console.log('unique users in db ', users);
     res.json(users);
   });
 })
